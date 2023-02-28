c7935724388359e07b1e9c9583ad20dd0c8e93e9
Nitin <unlikenitin@gmail.com>
2023-02-28 13:01:04 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  const [display, setDisplay] = useState("");
-  const [expression, setExpression] = useState([]);
+  const [display, setDisplay] = useState("");  //here [display-input value ,setDisplay-the changed value] and useState(default value) is a hook ("") this is a string 
+  const [expression, setExpression] = useState([]); //same as above just ([]) this means it is an array
 
   const handleClick = value => {
     setDisplay(value);
     setExpression([...expression, value]);
-  };
+  }; //function used to set the value on click
 
   const handleResult = () => {
-    const result = expression
-      .join("")
-      .split(/(\D)/g)
-      .map(value => (value.match(/\d/g) ? parseInt(value, 0) : value))
-      .reduce((acc, value, index, array) => {
+    const result = expression.join("").split(/(\D)/g).map(value => (value.match(/\d/g) ? parseInt(value, 0) : value))
+    .reduce((acc, value, index, array) => {
         switch (value) {
           case "+":
             return (acc = acc + array[index + 1]);
@@ -72,4 +69,3 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
